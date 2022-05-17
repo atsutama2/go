8e29f139d7b7a350d2cd3b63e7e05785888802d1
atsutama2 <atsutama.0713@gmail.com>
2022-05-17 12:06:38 +0900

    go test

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World");
+func main() {
+	//fmt.Println("Hello World")
+	//fmt.Print("Hello World!!")
+	//fmt.Fprint(os.Stdout, "os Stdout")
+	//fmt.Fprintln(os.Stdout, "os Stdout")
+	hello := fmt.Sprint("fmt.Sprint")
+	fmt.Println(hello)
 }
